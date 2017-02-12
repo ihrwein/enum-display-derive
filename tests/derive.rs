72b0730c4e7836356e39a15532c57a01848c5e52
Tibor Benke <ihrwein@gmail.com>
2017-02-12 14:35:36 +0100

    refact(tests): rename FizzBuzz to FooBar
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- tests/derive.rs
+++ tests/derive.rs
@@ -6,15 +6,15 @@ use std::fmt::Display;
 #[test]
 fn test_derive() {
     #[derive(Display)]
-    enum FizzBuzz {
-        Fizz,
-        Buzz,
-        Bar(),
+    enum FooBar {
+        Foo,
+        Bar,
+        FooBar(),
         Number(i32),
     }
 
-    assert_eq!(String::from("Fizz"), format!("{}", FizzBuzz::Fizz));
-    assert_eq!(String::from("Buzz"), format!("{}", FizzBuzz::Buzz));
-    assert_eq!(String::from("Bar()"), format!("{}", FizzBuzz::Bar()));
-    assert_eq!(String::from("42"), format!("{}", FizzBuzz::Number(42)));
+    assert_eq!(String::from("Foo"), format!("{}", FooBar::Foo));
+    assert_eq!(String::from("Bar"), format!("{}", FooBar::Bar));
+    assert_eq!(String::from("FooBar()"), format!("{}", FooBar::FooBar()));
+    assert_eq!(String::from("42"), format!("{}", FooBar::Number(42)));
 }
